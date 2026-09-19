7f4026cf0119761a9eb21cba6bdc0e03e94c87b8
Your Name <you@example.com>
2026-09-19 22:34:45 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 6
-        versionName = "2.0"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
